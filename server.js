096aafa8c1a68f1840d095cb46a1b7d34e390e7e
Malu Ram <maluramgurjar64@gmail.com>
2025-08-21 16:44:00 +0530

    url  change

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const corsOrigin = process.env.CLIENT_ORIGIN || "*";
 
 // Allow frontend domain for all routes
 app.use(cors({
-  origin: "http://localhost:5173", // your frontend URL
+  origin: "https:tommalu.netlify.app", // your frontend URL
   credentials: true
 }));
 

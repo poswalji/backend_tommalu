8a15531b787692ef4f5fa6c9d0f51762d06300bb
Malu Ram <maluramgurjar64@gmail.com>
2025-08-21 18:44:04 +0530

    /protect

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,3 @@
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
-
 export const protect = async (req, res, next) => {
   try {
     const auth = req.headers.authorization || "";
@@ -12,7 +9,7 @@ export const protect = async (req, res, next) => {
     if (!user) return res.status(401).json({ message: "User not found" });
     if (user.status === "disabled") return res.status(403).json({ message: "Account disabled" });
 
-    req.user = user;
+    req.user = user; // authenticated user info attach
     next();
   } catch (err) {
     return res.status(401).json({ message: "Token invalid" });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { register, login, verifyToken, logout, googleAuth,updateProfile } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = Router();
 //udate //
 
-router.put("/profile", updateProfile);
+router.put("/profile",protect, updateProfile);
 
 // Email-Password Auth
 router.post("/register", register);
